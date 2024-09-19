bf04009facb0e8819f8dac63d9a908ca90f6bafa
ghaisan <danish@kali>
2024-09-19 07:49:17 +0700

    slow bg loading fix

--- app/_components/BgIMG.tsx
+++ app/_components/BgIMG.tsx
@@ -1,12 +1,14 @@
 import Image, { StaticImageData } from "next/image";
 export default function BGIMG({ src }: { src: StaticImageData }) {
-	return (
-		<Image
-			width="3200"
-			height="1800"
-			src={src}
-			alt=""
-			className="absolute -z-10 top-0 left-0 h-full  object-cover md:object-fill"
-		/>
-	);
+  return (
+    <Image
+      width="3200"
+      height="1800"
+      src={src}
+      alt=""
+      className="absolute -z-10 top-0 left-0 h-full  object-cover md:object-fill"
+      priority={true}
+      placeholder="blur"
+    />
+  );
 }
