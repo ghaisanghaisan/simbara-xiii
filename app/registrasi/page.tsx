519dc49426933de70fb63107e7b8f8e4a9a2dcbf
ghaisan <danish@kali>
2024-10-19 18:32:27 +0700

    return to formbackend

--- app/registrasi/page.tsx
+++ app/registrasi/page.tsx
@@ -269,8 +269,8 @@ export default function Registrasi() {
           </li>
         </ol>
       </div>
-      <form method="POST" onSubmit={onSubmit} className="space-y-6 mt-12" encType="multipart/form-data">
-        {/*<form method="POST" action="https://www.formbackend.com/f/72a939de54345d67" className="space-y-6 mt-12" encType="multipart/form-data">*/}
+      {/*<form method="POST" onSubmit={onSubmit} className="space-y-6 mt-12" encType="multipart/form-data">*/}
+      <form method="POST" action="https://www.formbackend.com/f/72a939de54345d67" className="space-y-6 mt-12" encType="multipart/form-data">
         <InputSection
           title="Nama Perwakilan"
           name="Nama"
@@ -356,6 +356,6 @@ export default function Registrasi() {
           Submit
         </button>
       </form>
-    </section>
+    </section >
   );
 }
